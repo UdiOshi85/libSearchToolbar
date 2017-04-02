febe69c09ed91698fca6e645f6e15c8525f28a83
udioshi-work <udioshi@gmail.com>
2017-04-02 18:19:05 +0300

    + Fixed some issues

--- src/main/java/com/github/udioshi85/libsearchtoolbar/SearchAnimationToolbar.java
+++ src/main/java/com/github/udioshi85/libsearchtoolbar/SearchAnimationToolbar.java
@@ -10,6 +10,8 @@ import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,29 +24,38 @@ import android.widget.ImageView;
  * Created on 4/2/2017.
  */
 
-public class SearchToolbar extends FrameLayout {
+public class SearchAnimationToolbar extends FrameLayout implements TextWatcher {
 
     static final String TAG = "SearchToolbar";
+
+    public interface OnSearchQueryChangedListener {
+        void onSearchCollapsed();
+        void onSearchQueryChanged(String query);
+        void onSearchExpanded();
+    }
+
 
     private Toolbar toolbar;
     private Toolbar searchToolbar;
     private MenuItem searchMenuItem;
+    private OnSearchQueryChangedListener onSearchQueryChangedListener;
+    private String currentQuery = "";
 
-    public SearchToolbar(Context context) {
+    public SearchAnimationToolbar(Context context) {
         this(context, null);
     }
 
-    public SearchToolbar(Context context, @Nullable AttributeSet attrs) {
+    public SearchAnimationToolbar(Context context, @Nullable AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    public SearchToolbar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public SearchAnimationToolbar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         inflateAndBindViews();
     }
 
     private void inflateAndBindViews() {
-        View.inflate(getContext(), R.layout.view_search_toolbar, SearchToolbar.this);
+        View.inflate(getContext(), R.layout.view_search_toolbar, SearchAnimationToolbar.this);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         searchToolbar = (Toolbar) findViewById(R.id.searchToolbar);
@@ -96,7 +107,17 @@ public class SearchToolbar extends FrameLayout {
 
         EditText txtSearch = ((EditText) searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text));
         txtSearch.setHint("Search contacts & places");
+        txtSearch.addTextChangedListener(SearchAnimationToolbar.this);
 
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        final SearchView searchView = (SearchView) searchMenuItem.getActionView();
+        EditText txtSearch = ((EditText) searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text));
+        txtSearch.setHint("Search contacts & places");
+        txtSearch.removeTextChangedListener(SearchAnimationToolbar.this);
+        super.onDetachedFromWindow();
     }
 
     public void setSupportActionBar(AppCompatActivity act) {
@@ -115,15 +136,12 @@ public class SearchToolbar extends FrameLayout {
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void circleReveal(final boolean isShow) {
-
-        int positionFromRight = 1;
+        
         int width = toolbar.getWidth();
-
-        width -= (positionFromRight * getResources().getDimensionPixelSize(R.dimen.abc_action_button_min_width_material)) - (getResources().getDimensionPixelSize(R.dimen.abc_action_button_min_width_material) / 2);
-        width -= getResources().getDimensionPixelSize(R.dimen.abc_action_button_min_width_overflow_material);
+        width -= (getResources().getDimensionPixelSize(R.dimen.abc_action_button_min_width_material) / 2);
 
         int cx = width;
-        int cy = searchToolbar.getHeight() / 2;
+        int cy = toolbar.getHeight() / 2;
 
         Animator anim;
         if (isShow)
@@ -147,10 +165,10 @@ public class SearchToolbar extends FrameLayout {
         // make the view visible and start the animation
         if (isShow) {
             searchToolbar.setVisibility(View.VISIBLE);
+            notifySearchExpanded();
         } else {
-
+            notifySearchCallapsed();
         }
-
 
         // start the animation
         anim.start();
@@ -172,5 +190,44 @@ public class SearchToolbar extends FrameLayout {
 
     public Toolbar getToolbar() {
         return toolbar;
+    }
+
+    public void setOnSearchQueryChangedListener(OnSearchQueryChangedListener onSearchQueryChangedListener) {
+        this.onSearchQueryChangedListener = onSearchQueryChangedListener;
+    }
+
+    private void notifySearchCallapsed() {
+        if (this.onSearchQueryChangedListener != null) {
+            this.onSearchQueryChangedListener.onSearchCollapsed();
+        }
+    }
+
+    private void notifySearchExpanded() {
+        if (this.onSearchQueryChangedListener != null) {
+            this.onSearchQueryChangedListener.onSearchExpanded();
+        }
+    }
+
+    private void notifySearchQueryChanged(String q) {
+        if (this.onSearchQueryChangedListener != null) {
+            this.onSearchQueryChangedListener.onSearchQueryChanged(q);
+        }
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+        if (!currentQuery.equalsIgnoreCase(s.toString())) {
+            notifySearchQueryChanged(s.toString());
+        }
+
     }
 }
