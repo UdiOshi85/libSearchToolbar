cf31a3412c71b9db04bddc10eed538928a9d4ff2
udi oshi <udioshi@gmail.com>
2017-04-18 17:26:13 +0300

    + Added attributes
    + Upgrade to com.android.support:appcompat-v7:25.3.1

--- src/main/java/com/oshi/libsearchtoolbar/SearchAnimationToolbar.java
+++ src/main/java/com/oshi/libsearchtoolbar/SearchAnimationToolbar.java
@@ -3,6 +3,8 @@ package com.oshi.libsearchtoolbar;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.os.Build;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
@@ -11,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.MenuItem;
@@ -20,23 +23,29 @@ import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
-/**
- * Created on 4/2/2017.
- */
+import org.w3c.dom.Text;
 
 public class SearchAnimationToolbar extends FrameLayout implements TextWatcher {
 
-    static final String TAG = "SearchToolbar";
-
     public interface OnSearchQueryChangedListener {
         void onSearchCollapsed();
+
         void onSearchQueryChanged(String query);
+
         void onSearchExpanded();
     }
+
+    private String toolbarTitle;
+    private String toolbarSubtitle;
+    private String searchHint;
+    private int toolbarTitleColor;
+    private int toolbarSubtitleColor;
+    private int searchBackgroundColor;
 
 
     private Toolbar toolbar;
     private Toolbar searchToolbar;
+    private EditText txtSearch;
     private MenuItem searchMenuItem;
     private OnSearchQueryChangedListener onSearchQueryChangedListener;
     private String currentQuery = "";
@@ -52,7 +61,36 @@ public class SearchAnimationToolbar extends FrameLayout implements TextWatcher {
     public SearchAnimationToolbar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         inflateAndBindViews();
+        bindAttributes(attrs);
     }
+
+    private void bindAttributes(AttributeSet attrs) {
+
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.SearchAnimationToolbar);
+        toolbarTitle = typedArray.getString(R.styleable.SearchAnimationToolbar_title);
+        toolbarTitleColor = typedArray.getColor(R.styleable.SearchAnimationToolbar_titleTextColor, Color.WHITE);
+        toolbarSubtitle = typedArray.getString(R.styleable.SearchAnimationToolbar_subtitle);
+        toolbarSubtitleColor = typedArray.getColor(R.styleable.SearchAnimationToolbar_subtitleTextColor, Color.WHITE);
+
+        toolbar.setTitle(toolbarTitle);
+        toolbar.setTitleTextColor(toolbarTitleColor);
+
+        if (!TextUtils.isEmpty(toolbarSubtitle)) {
+            toolbar.setSubtitle(toolbarSubtitle);
+            toolbar.setSubtitleTextColor(toolbarSubtitleColor);
+        }
+
+        searchHint = typedArray.getString(R.styleable.SearchAnimationToolbar_searchHint);
+
+        if (!TextUtils.isEmpty(searchHint)) {
+            txtSearch.setHint(searchHint);
+        }
+
+        searchBackgroundColor = typedArray.getColor(R.styleable.SearchAnimationToolbar_searchBackroundColor, Color.WHITE);
+        searchToolbar.setBackgroundColor(searchBackgroundColor);
+
+    }
+
 
     private void inflateAndBindViews() {
         View.inflate(getContext(), R.layout.view_search_toolbar, SearchAnimationToolbar.this);
@@ -60,34 +98,26 @@ public class SearchAnimationToolbar extends FrameLayout implements TextWatcher {
         toolbar = (Toolbar) findViewById(R.id.lib_search_animation_toolbar);
         searchToolbar = (Toolbar) findViewById(R.id.lib_search_animation_search_toolbar);
 
-        searchToolbar.inflateMenu(R.menu.menu_search);
+        searchToolbar.inflateMenu(R.menu.lib_search_toolbar_menu_search);
         searchToolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    circleReveal(false);
-                } else {
-                    searchToolbar.setVisibility(View.GONE);
-                }
+                collapse();
             }
         });
 
-        searchMenuItem = searchToolbar.getMenu().findItem(R.id.action_filter_search);
+        searchMenuItem = searchToolbar.getMenu().findItem(R.id.lib_search_toolbar_action_filter_search);
 
         MenuItemCompat.setOnActionExpandListener(searchMenuItem, new MenuItemCompat.OnActionExpandListener() {
             @Override
             public boolean onMenuItemActionCollapse(MenuItem item) {
                 // Do something when collapsed
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    circleReveal(false);
-                } else
-                    searchToolbar.setVisibility(View.GONE);
+                collapse();
                 return true;
             }
 
             @Override
             public boolean onMenuItemActionExpand(MenuItem item) {
-                // Do something when expanded
                 return true;
             }
         });
@@ -104,18 +134,13 @@ public class SearchAnimationToolbar extends FrameLayout implements TextWatcher {
 
 
         // set hint and the text colors
-
-        EditText txtSearch = ((EditText) searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text));
-        txtSearch.setHint("Search contacts & places");
+        txtSearch = ((EditText) searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text));
         txtSearch.addTextChangedListener(SearchAnimationToolbar.this);
 
     }
 
     @Override
     protected void onDetachedFromWindow() {
-        final SearchView searchView = (SearchView) searchMenuItem.getActionView();
-        EditText txtSearch = ((EditText) searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text));
-        txtSearch.setHint("Search contacts & places");
         txtSearch.removeTextChangedListener(SearchAnimationToolbar.this);
         super.onDetachedFromWindow();
     }
@@ -125,18 +150,14 @@ public class SearchAnimationToolbar extends FrameLayout implements TextWatcher {
     }
 
     public boolean onSearchIconClick() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            circleReveal(true);
-        } else {
-            searchToolbar.setVisibility(View.VISIBLE);
-        }
+        expand();
         searchMenuItem.expandActionView();
         return true;
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void circleReveal(final boolean isShow) {
-        
+
         int width = toolbar.getWidth();
         width -= (getResources().getDimensionPixelSize(R.dimen.abc_action_button_min_width_material) / 2);
 
@@ -175,13 +196,24 @@ public class SearchAnimationToolbar extends FrameLayout implements TextWatcher {
     }
 
     public void setSearchHint(String hint) {
-        final SearchView searchView = (SearchView) searchMenuItem.getActionView();
-        EditText txtSearch = ((EditText) searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text));
         txtSearch.setHint(hint);
     }
 
+    public boolean onBackPressed() {
+        boolean isInSearchMode = searchToolbar.getVisibility() == View.VISIBLE;
+        if (!isInSearchMode) {
+            return false;
+        }
+
+        collapse();
+        searchMenuItem.collapseActionView();
+        return true;
+    }
+
+
     public void setTitle(String title) {
         toolbar.setTitle(title);
+        toolbar.invalidate();
     }
 
     public void setTitleTextColor(int color) {
@@ -190,6 +222,10 @@ public class SearchAnimationToolbar extends FrameLayout implements TextWatcher {
 
     public Toolbar getToolbar() {
         return toolbar;
+    }
+
+    public Toolbar getSearchToolbar() {
+        return searchToolbar;
     }
 
     public void setOnSearchQueryChangedListener(OnSearchQueryChangedListener onSearchQueryChangedListener) {
@@ -227,6 +263,21 @@ public class SearchAnimationToolbar extends FrameLayout implements TextWatcher {
 
         if (!currentQuery.equalsIgnoreCase(s.toString())) {
             notifySearchQueryChanged(s.toString());
+        }
+    }
+
+    private void collapse() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            circleReveal(false);
+        } else
+            searchToolbar.setVisibility(View.GONE);
+    }
+
+    private void expand() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            circleReveal(true);
+        } else {
+            searchToolbar.setVisibility(View.VISIBLE);
         }
     }
 }
